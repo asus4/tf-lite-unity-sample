8cb84d2da6b012019203d37b32010a4804d79fa1
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-14 13:58:43 +0100

    Add mnist samples

---
+++ Assets/Samples/MNIST/LineDrawer.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class LineDrawer : MonoBehaviour, IDragHandler
+{
+
+
+    [System.Serializable]
+    public class DrawEvent : UnityEvent<RenderTexture>
+    {
+    }
+
+
+    [SerializeField] Vector2Int imageDimention = new Vector2Int(28, 28);
+    [SerializeField] Color paintColor = Color.white;
+    [SerializeField] RenderTextureFormat format = RenderTextureFormat.R8;
+
+    [SerializeField] DrawEvent OnDraw = new DrawEvent();
+
+
+    RawImage imageView = null;
+
+    Mesh lineMesh;
+    Material lineMaterial;
+    Texture2D clearTexture;
+
+    RenderTexture texture;
+
+    void OnEnable()
+    {
+        imageView = GetComponent<RawImage>();
+
+        lineMesh = new Mesh();
+        lineMesh.MarkDynamic();
+        lineMesh.vertices = new Vector3[2];
+        lineMesh.SetIndices(new[] { 0, 1 }, MeshTopology.Lines, 0);
+        lineMaterial = new Material(Shader.Find("Hidden/LineShader"));
+        lineMaterial.SetColor("_Color", paintColor);
+
+        texture = new RenderTexture(imageDimention.x, imageDimention.y, 0, format);
+        texture.filterMode = FilterMode.Bilinear;
+        imageView.texture = texture;
+
+        clearTexture = Texture2D.blackTexture;
+
+        var trigger = GetComponent<EventTrigger>();
+    }
+
+    void Start()
+    {
+        ClearTexture();
+    }
+
+    void OnDisable()
+    {
+        texture?.Release();
+        Destroy(lineMesh);
+        Destroy(lineMaterial);
+    }
+
+    public void ClearTexture()
+    {
+        Graphics.Blit(clearTexture, texture);
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        data.Use();
+
+        var area = data.pointerDrag.GetComponent<RectTransform>();
+        var p0 = area.InverseTransformPoint(data.position - data.delta);
+        var p1 = area.InverseTransformPoint(data.position);
+
+        var scale = new Vector3(2 / area.rect.width, -2 / area.rect.height, 0);
+        p0 = Vector3.Scale(p0, scale);
+        p1 = Vector3.Scale(p1, scale);
+
+        DrawLine(p0, p1);
+
+
+        OnDraw.Invoke(texture);
+    }
+
+    void DrawLine(Vector3 p0, Vector3 p1)
+    {
+        var prevRT = RenderTexture.active;
+        RenderTexture.active = texture;
+
+        lineMesh.SetVertices(new List<Vector3>() { p0, p1 });
+        lineMaterial.SetPass(0);
+        Graphics.DrawMeshNow(lineMesh, Matrix4x4.identity);
+
+        RenderTexture.active = prevRT;
+    }
+}
+
---
+++ Assets/Samples/MNIST/MNIST.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+
+public class MNIST : MonoBehaviour
+{
+    [SerializeField] string fileName = "mnist.tflite";
+    [SerializeField] Text outputTextView = null;
+    [SerializeField] ComputeShader compute = null;
+
+    Interpreter interpreter;
+
+    bool isProcessing = false;
+    float[] inputs = new float[28 * 28];
+    float[] outputs = new float[10];
+    ComputeBuffer inputBuffer;
+    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+    void Start()
+    {
+        var path = Path.Combine(Application.streamingAssetsPath, fileName);
+        interpreter = new Interpreter(File.ReadAllBytes(path));
+        interpreter.ResizeInputTensor(0, new int[] { 1, 28, 28, 1 });
+        interpreter.AllocateTensors();
+
+        inputBuffer = new ComputeBuffer(28 * 28, sizeof(float));
+    }
+
+    void OnDestroy()
+    {
+        interpreter?.Dispose();
+
+        inputBuffer?.Dispose();
+    }
+
+    public void OnDrawTexture(RenderTexture texture)
+    {
+        if (!isProcessing)
+        {
+            ExcecuteAsync(texture);
+        }
+    }
+
+    void ExcecuteAsync(RenderTexture texture)
+    {
+        isProcessing = true;
+
+        compute.SetTexture(0, "InputTexture", texture);
+        compute.SetBuffer(0, "OutputTensor", inputBuffer);
+        compute.Dispatch(0, 28 / 4, 28 / 4, 1);
+        inputBuffer.GetData(inputs);
+
+
+        float startTime = Time.realtimeSinceStartup;
+        interpreter.SetInputTensorData(0, inputs);
+        interpreter.Invoke();
+        interpreter.GetOutputTensorData(0, outputs);
+        float duration = Time.realtimeSinceStartup - startTime;
+
+        sb.Clear();
+        sb.AppendLine($"Process time: {duration: 0.00000}");
+        sb.AppendLine("---");
+        for (int i = 0; i < outputs.Length; i++)
+        {
+            sb.AppendLine($"{i}: {outputs[i]: 0.00}");
+        }
+        outputTextView.text = sb.ToString();
+
+        isProcessing = false;
+    }
+}
