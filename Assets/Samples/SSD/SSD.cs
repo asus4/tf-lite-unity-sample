5a4a94a7a7ad4c8b26fbaf96033570925fe31e69
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-14 22:35:33 +0100

    WIP SSD

---
+++ Assets/Samples/SSD/SSD.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace TensorFlowLite
+{
+    public class SSD : System.IDisposable
+    {
+        public struct Result
+        {
+            public int classID;
+            public float score;
+            public Rect rect;
+        }
+
+        const int WIDTH = 300;
+        const int HEIGHT = 300;
+        const int CHANNELS = 3; // RGB
+
+        Interpreter interpreter;
+        ComputeShader compute;
+        ComputeBuffer inputBuffer;
+
+        // https://www.tensorflow.org/lite/models/object_detection/overview
+        sbyte[] inputs = new sbyte[WIDTH * HEIGHT * CHANNELS];
+        float[] outputs0 = new float[10 * 4]; // RECTs
+        float[] outputs1 = new float[10]; // Classes
+        float[] outputs2 = new float[10]; // Scores
+        Result[] results = new Result[10];
+
+        public SSD(string modelPath, ComputeShader compute)
+        {
+            this.compute = compute;
+
+            interpreter = new Interpreter(File.ReadAllBytes(modelPath));
+            interpreter.ResizeInputTensor(0, new int[] { 1, HEIGHT, WIDTH, CHANNELS });
+            interpreter.AllocateTensors();
+
+            inputBuffer = new ComputeBuffer(WIDTH * HEIGHT * CHANNELS, sizeof(sbyte)); // uint8
+            inputs = new sbyte[WIDTH * HEIGHT * CHANNELS];
+        }
+
+        public void Dispose()
+        {
+            interpreter?.Dispose();
+            inputBuffer?.Dispose();
+        }
+
+        public void Invoke(Texture texture)
+        {
+            compute.SetTexture(0, "InputTexture", texture);
+            compute.SetBuffer(0, "OutputTensor", inputBuffer);
+            compute.Dispatch(0, 300 / 10, 300 / 10, 1);
+
+            inputBuffer.GetData(inputs);
+
+            float startSec = Time.realtimeSinceStartup;
+            interpreter.SetInputTensorData(0, inputs);
+            interpreter.Invoke();
+            interpreter.GetOutputTensorData(0, outputs0);
+            interpreter.GetOutputTensorData(1, outputs1);
+            interpreter.GetOutputTensorData(2, outputs2);
+
+            float durationMs = (int)((Time.realtimeSinceStartup - startSec) * 1000);
+            Debug.Log($"{durationMs} ms");
+        }
+
+        public Result[] GetResults()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                float top = outputs0[i * 4];
+                float left = outputs0[i * 4 + 1];
+                float bottom = outputs0[i * 4 + 2];
+                float right = outputs0[i * 4 + 3];
+
+                results[i] = new Result()
+                {
+                    classID = (int)outputs1[i],
+                    score = outputs2[i],
+                    rect = new Rect(left, top, right - left, bottom - top),
+                };
+            }
+            return results;
+        }
+    }
+
+}
---
+++ Assets/Samples/SSD/SsdSample.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+public class SsdSample : MonoBehaviour
+{
+    [SerializeField] string fileName = "coco_ssd_mobilenet_quant.tflite";
+    [SerializeField] RawImage cameraView = null;
+    [SerializeField] ComputeShader compute = null;
+    [SerializeField] Text framePrefab = null;
+    [SerializeField] float scoreThreshold = 0.5f;
+    [SerializeField] TextAsset labelMap = null;
+
+    WebCamTexture webcamTexure;
+    SSD ssd;
+
+    Text[] frames;
+    public string[] labels;
+
+    void Start()
+    {
+
+        var path = Path.Combine(Application.streamingAssetsPath, fileName);
+        ssd = new SSD(path, compute);
+
+        // Init camera
+        var cameraName = WebCamTexture.devices.Last().name;
+        webcamTexure = new WebCamTexture(cameraName, 1280, 720);
+        cameraView.texture = webcamTexure;
+        webcamTexure.Play();
+        Debug.Log($"Starting camera: {cameraName}");
+
+        // Init frames
+        frames = new Text[10];
+        var parent = cameraView.transform;
+        for (int i = 0; i < frames.Length; i++)
+        {
+            frames[i] = Instantiate(framePrefab, Vector3.zero, Quaternion.identity, parent) as Text;
+        }
+
+        // Labels
+        labels = labelMap.text.Split('\n');
+    }
+
+    void OnDestroy()
+    {
+        ssd?.Dispose();
+    }
+
+    void Update()
+    {
+        ssd.Invoke(webcamTexure);
+
+        var size = ((RectTransform)cameraView.transform).rect.size;
+
+        var results = ssd.GetResults();
+        for (int i = 0; i < 10; i++)
+        {
+            SetFrame(frames[i], results[i], size);
+        }
+    }
+
+    void SetFrame(Text frame, SSD.Result result, Vector2 size)
+    {
+
+        if (result.score < scoreThreshold)
+        {
+            frame.gameObject.SetActive(false);
+            return;
+        }
+        else
+        {
+            frame.gameObject.SetActive(true);
+        }
+
+        frame.text = $"{GetLabelName(result.classID)} : {result.score}";
+        var rt = frame.transform as RectTransform;
+        rt.anchoredPosition = result.rect.position * size;
+        rt.sizeDelta = result.rect.size * size;
+    }
+
+    string GetLabelName(int id)
+    {
+        if (id <= 0 || id >= labels.Length)
+        {
+            return "?";
+        }
+        return labels[id];
+    }
+
+}
