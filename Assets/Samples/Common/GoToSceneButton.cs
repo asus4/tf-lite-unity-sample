b6818e3392e943df14b0648670bab591984d6ce7
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-26 23:32:38 +0100

    Add Index scene

---
+++ Assets/Samples/Common/GoToSceneButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace TensorFlowLite
+{
+    [RequireComponent(typeof(Button))]
+    public class GoToSceneButton : MonoBehaviour
+    {
+        public string sceneName = "";
+        public LoadSceneMode mode = LoadSceneMode.Single;
+
+        void OnEnable()
+        {
+            GetComponent<Button>().onClick.AddListener(OnButtonClick);
+        }
+
+        void OnDisable()
+        {
+            GetComponent<Button>().onClick.RemoveListener(OnButtonClick);
+        }
+
+        void OnButtonClick()
+        {
+            SceneManager.LoadScene(sceneName, mode);
+        }
+    }
+}
