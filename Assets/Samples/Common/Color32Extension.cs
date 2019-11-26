c89ecb39797bc0c42ff63fb386a3fdc7f3168bee
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-26 23:22:37 +0100

    Use compute shader

--- Assets/Samples/Common/Color32Extension.cs
+++ Assets/Samples/Common/Color32Extension.cs
@@ -18,10 +18,10 @@ namespace TensorFlowLite
         public static float4 ToRGBA(this Color32 c)
         {
             return new float4(
-                unchecked((sbyte)c.r) / 256f,
-                unchecked((sbyte)c.g) / 256f,
-                unchecked((sbyte)c.b) / 256f,
-                unchecked((sbyte)c.a) / 256f
+                (float)unchecked((sbyte)c.r) / 255f,
+                (float)unchecked((sbyte)c.g) / 255f,
+                (float)unchecked((sbyte)c.b) / 255f,
+                (float)unchecked((sbyte)c.a) / 255f
             );
         }
 
