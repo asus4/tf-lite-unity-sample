39641b1549ebdc2f5fc566aeada1174a6280b8ba
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-27 12:51:56 +0100

    Add styletransfer files

---
+++ Assets/Samples/StyleTransfer/StylePredict.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TensorFlowLite
+{
+    public class StylePredict : BaseImagePredictor<float>
+    {
+        float[] output0;
+
+        public StylePredict(string modelPath) : base(modelPath)
+        {
+            var outDim0 = interpreter.GetOutputTensorInfo(0).dimensions;
+            output0 = new float[outDim0[3]]; // shold be 100
+        }
+
+        public override void Invoke(Texture inputTex)
+        {
+            ToTensor(inputTex, inputs);
+
+            interpreter.SetInputTensorData(0, inputs);
+            interpreter.Invoke();
+            interpreter.GetOutputTensorData(0, output0);
+        }
+
+        public float[] GetStyleBottleneck()
+        {
+            return output0;
+        }
+    }
+}
---
+++ Assets/Samples/StyleTransfer/StyleTransferSample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+public class StyleTransferSample : MonoBehaviour
+{
+    [SerializeField] string predictionFileName = "style_predict_quantized_256.tflite";
+    [SerializeField] string transferFileName = "style_transfer_quantized_dynamic.tflite";
+    [SerializeField] Texture2D styleImage = null;
+    [SerializeField] RawImage preview = null;
+
+    public float[] styleBottleneck;
+
+    void Start()
+    {
+        string predictionModelPath = Path.Combine(Application.streamingAssetsPath, predictionFileName);
+        using (var predict = new StylePredict(predictionModelPath))
+        {
+            predict.Invoke(styleImage);
+            styleBottleneck = predict.GetStyleBottleneck();
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+}
