be1b8f2cedc2d4c547f5ad8fa1bcfebd368e0327
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-27 12:51:56 +0100

    Add styletransfer files

---
+++ Assets/Samples/StyleTransfer/StyleTransfer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace TensorFlowLite
+{
+    public class StyleTransfer : System.IDisposable
+    {
+        Interpreter interpreter;
+
+        public StyleTransfer(string modelPath)
+        {
+            interpreter = new Interpreter(File.ReadAllBytes(modelPath));
+        }
+
+        public void Dispose()
+        {
+            interpreter?.Dispose();
+        }
+    }
+}
