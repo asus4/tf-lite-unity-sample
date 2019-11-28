e1df757d6a231405042540ff315db12ef8d7e75a
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-28 23:02:46 +0100

    Add Camera Authorization

---
+++ Assets/Samples/Index/IndexSample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexSample : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        // Need the WebCam Authorization before using Camera on mobile devices
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        }
+    }
+
+}
