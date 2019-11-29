31c7f0efd89014e590e9d13edf1cf2a072b451b3
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-11-29 11:06:29 +0100

    Add gpu delegate bridge for android

--- Assets/TensorFlowLite/SDK/Scripts/GpuDelegate.cs
+++ Assets/TensorFlowLite/SDK/Scripts/GpuDelegate.cs
@@ -13,14 +13,55 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-using System;
+using System.Runtime.InteropServices;
 
 using TfLiteDelegate = System.IntPtr;
 
 namespace TensorFlowLite
 {
-    public interface IGpuDelegate : IDisposable
+#if UNITY_ANDROID && !UNITY_EDITOR
+
+    /// <summary>
+    /// TfLiteGpuDelegateOptionsV2
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Options
     {
-        TfLiteDelegate Delegate { get; }
+        int isPrecisionLossAllowed;
+        int inferencePreference;
+        int inferencePriority1;
+        int inferencePriority2;
+        int inferencePriority3;
+    };
+
+    public class GpuDelegate : IGpuDelegate
+    {
+        public TfLiteDelegate Delegate { get; private set; }
+
+        public GpuDelegate()
+        {
+            Options options = TfLiteGpuDelegateOptionsV2Default();
+            Delegate = TfLiteGpuDelegateV2Create(options);
+        }
+
+        public void Dispose()
+        {
+            TfLiteGpuDelegateV2Delete(Delegate);
+            Delegate = TfLiteDelegate.Zero;
+        }
+
+    #region Externs
+        private const string TensorFlowLibraryGPU = "libtensorflowlite_gpu_delegate";
+
+        [DllImport(TensorFlowLibraryGPU)]
+        private static extern unsafe Options TfLiteGpuDelegateOptionsV2Default();
+
+        [DllImport(TensorFlowLibraryGPU)]
+        private static extern unsafe TfLiteDelegate TfLiteGpuDelegateV2Create(Options options);
+
+        [DllImport(TensorFlowLibraryGPU)]
+        private static extern unsafe void TfLiteGpuDelegateV2Delete(TfLiteDelegate gpuDelegate);
+    #endregion // Externs
     }
+#endif // UNITY_ANDROID && !UNITY_EDITOR
 }
