29bb35dca1681bf9bc146acaa3aaeb21093574cc
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-12-06 01:29:40 +0100

    Add Text classification sample

---
+++ Assets/Samples/TextClassification/TextClassification.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TensorFlowLite
+{
+
+    public class TextClassification : System.IDisposable
+    {
+        public struct Result
+        {
+            public float negative;
+            public float positive;
+        }
+
+        enum Marker
+        {
+            PAD = 0,//      used for padding
+            START = 1,// mark for the start of a sentence
+            UNKNOWN = 2,//  mark for unknown words (OOV)
+        }
+
+        Interpreter interpreter;
+        float[] inputs;
+        float[] outputs;
+
+        public Result result { get; private set; }
+
+        Dictionary<string, int> vocabulary;
+
+        public TextClassification(string modelPath, string vocabularyText)
+        {
+            vocabulary = BuildVocabulary(vocabularyText);
+
+            // NO GPU
+            var options = new Interpreter.Options()
+            {
+                threads = 2
+            };
+            interpreter = new Interpreter(FileUtil.LoadFile(modelPath), options);
+
+            var inputInfo = interpreter.GetInputTensorInfo(0);
+            var outputInfo = interpreter.GetOutputTensorInfo(0);
+            inputs = new float[inputInfo.shape[1]];
+            outputs = new float[outputInfo.shape[1]];
+            interpreter.ResizeInputTensor(0, inputInfo.shape);
+            interpreter.AllocateTensors();
+        }
+
+        public void Dispose()
+        {
+            interpreter?.Dispose();
+        }
+
+        public void Invoke(string text)
+        {
+            TextToInput(text, inputs);
+
+            interpreter.SetInputTensorData(0, inputs);
+            interpreter.Invoke();
+            interpreter.GetOutputTensorData(0, outputs);
+
+            result = new Result()
+            {
+                negative = outputs[0],
+                positive = outputs[1],
+            };
+        }
+
+
+        void TextToInput(string text, float[] inputs)
+        {
+            text = text.ToLower();
+            char[] separator = { ',', '.', '!', '?', ' ', '\n' };
+            string[] words = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            // Start
+            int index = 0;
+            inputs[index] = (float)Marker.START;
+
+            // Fill 
+            foreach (var word in words)
+            {
+                index++;
+                if (index >= inputs.Length)
+                {
+                    break;
+                }
+                int val;
+                if (vocabulary.TryGetValue(word, out val))
+                {
+                    UnityEngine.Debug.Log($"{word} : {val}");
+                    inputs[index] = (float)val;
+                }
+                else
+                {
+                    UnityEngine.Debug.Log($"{word} : UNKNOWN");
+                    inputs[index] = (float)Marker.UNKNOWN;
+                }
+            }
+
+            // Fill Padding
+            for (; index < inputs.Length; index++)
+            {
+                inputs[index] = (float)Marker.PAD;
+            }
+
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sb.Append((int)inputs[i]);
+                sb.Append(' ');
+            }
+            UnityEngine.Debug.Log(sb);
+        }
+
+        static Dictionary<string, int> BuildVocabulary(string vocabularyText)
+        {
+
+            var sb = new System.Text.StringBuilder();
+
+            string[] lines = vocabularyText.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var dict = new Dictionary<string, int>();
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                dict.Add(words[0], int.Parse(words[1]));
+
+                sb.Append($"[{words[0]},{words[1]}]  ");
+            }
+
+            UnityEngine.Debug.Log(sb);
+            return dict;
+        }
+
+    }
+}
---
+++ Assets/Samples/TextClassification/TextClassificationSample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+public class TextClassificationSample : MonoBehaviour
+{
+    [SerializeField] string fileName = "deeplabv3_257_mv_gpu.tflite";
+    [SerializeField] TextAsset vocabularyText = null;
+    [SerializeField] InputField textInput = null;
+    [SerializeField] Text resultLabel = null;
+
+    TextClassification textClassification;
+
+    void Start()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, fileName);
+        textClassification = new TextClassification(path, vocabularyText.text);
+
+        OnTextChanged(textInput.text);
+    }
+
+    void OnDestroy()
+    {
+        textClassification?.Dispose();
+    }
+
+    public void OnTextChanged(string text)
+    {
+        Debug.Log(text);
+        textClassification.Invoke(text);
+        var result = textClassification.result;
+        resultLabel.text = $"Positive: {(result.positive * 100):0.0}%\n"
+                         + $"Negative: { (result.negative * 100):0.0}%";
+    }
+}
