74c81b2037fd0c9df577993a013c0e9e64c97dea
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-05-12 01:02:20 +0200

    Support iOS, Android

--- build_tflite.py
+++ build_tflite.py
@@ -23,22 +23,30 @@ def build_mac():
     run_cmd('bazel build -c opt --cxxopt=--std=c++11 tensorflow/lite/c:tensorflowlite_c')
     copy('bazel-bin/tensorflow/lite/c/libtensorflowlite_c.dylib', 'macOS/libtensorflowlite_c.dylib')
 
-    run_cmd('bazel build -c opt --copt -Os --copt -DTFLITE_GPU_BINARY_RELEASE --copt -fvisibility=hidden --linkopt -s --strip always --cxxopt=-std=c++14 --apple_platform_type=macos \'//tensorflow/lite/delegates/gpu:tensorflow_lite_gpu_dylib\'')
+    run_cmd('bazel build -c opt --copt -Os --copt -DTFLITE_GPU_BINARY_RELEASE --copt -fvisibility=hidden --linkopt -s --strip always --cxxopt=-std=c++14 --apple_platform_type=macos //tensorflow/lite/delegates/gpu:tensorflow_lite_gpu_dylib')
     copy('bazel-bin/tensorflow/lite/delegates/gpu/tensorflow_lite_gpu_dylib.dylib', 'macOS/libtensorflowlite_metal_delegate.dylib')
 
 def build_ios():
-    # run_cmd('bazel build -c opt --cxxopt=--std=c++11 tensorflow/lite/c:tensorflowlite_c')
-    # copy('bazel-bin/tensorflow/lite/c/libtensorflowlite_c.dylib', 'macOS/libtensorflowlite_c.dylib')
+    run_cmd('bazel build --config=ios_fat -c opt //tensorflow/lite/experimental/ios:TensorFlowLiteC_framework')
+    unzip('bazel-bin/tensorflow/lite/experimental/ios/TensorFlowLiteC_framework.zip', 'iOS')
 
-    run_cmd('bazel build -c opt --cpu ios_arm64 --copt -Os --copt -DTFLITE_GPU_BINARY_RELEASE --copt -fvisibility=hidden --copt=-fembed-bitcode --linkopt -s --strip always --cxxopt=-std=c++14 //tensorflow/lite/delegates/gpu:tensorflow_lite_gpu_framework --apple_platform_type=ios')
-    unzip('bazel-bin/tensorflow/lite/delegates/gpu/tensorflow_lite_gpu_framework.zip', 'ios')
+    run_cmd('bazel build -c opt --config=ios_fat --copt -Os --copt -DTFLITE_GPU_BINARY_RELEASE --copt -fvisibility=hidden --copt=-fembed-bitcode --linkopt -s --strip always --cxxopt=-std=c++14 //tensorflow/lite/delegates/gpu:tensorflow_lite_gpu_framework --apple_platform_type=ios')
+    unzip('bazel-bin/tensorflow/lite/delegates/gpu/tensorflow_lite_gpu_framework.zip', 'iOS')
 
+def build_android():
+    run_cmd('bazel build -c opt --cxxopt=--std=c++11 --config=android_arm64 //tensorflow/lite/c:libtensorflowlite_c.so')
+    copy('bazel-bin/tensorflow/lite/c/libtensorflowlite_c.so', 'Android')
+    
+    run_cmd('bazel build -c opt --config android_arm64 --copt -Os --copt -DTFLITE_GPU_BINARY_RELEASE --copt -fvisibility=hidden --linkopt -s --strip always //tensorflow/lite/delegates/gpu:libtensorflowlite_gpu_delegate.so')
+    copy('bazel-bin/tensorflow/lite/delegates/gpu/libtensorflowlite_gpu_delegate.so', 'Android')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build TensorFlow libraries for Unity')
     parser.add_argument('--tfpath',default='../tensorflow')
     parser.add_argument('-macos', action="store_true", default=False)
     parser.add_argument('-ios', action="store_true", default=False)
+    parser.add_argument('-android', action="store_true", default=False)
+
 
     args = parser.parse_args()
     TENSORFLOW_PATH = os.path.abspath(args.tfpath) 
@@ -48,6 +56,11 @@ if __name__ == '__main__':
         build_mac()
     
     if args.ios:
-        print('Build ios')
+        # Need to set iOS build option in ./configure
+        print('Build iOS')
         build_ios()
     
+    if args.android:
+        # Need to set Android build option in ./configure
+        print('Build Android')
+        build_android()
