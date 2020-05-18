a5e9fca95fcb2402fa6149dad167948fcfbfff48
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-05-18 12:58:09 +0200

    Fix color space in style transfer

--- Assets/Samples/StyleTransfer/StyleTransfer.cs
+++ Assets/Samples/StyleTransfer/StyleTransfer.cs
@@ -52,43 +52,12 @@ namespace TensorFlowLite
 
         public RenderTexture GetResultTexture()
         {
-            // Find min max;
-            float3 min, max;
-            FindMinMax(output0, out min, out max);
-
             outputBuffer.SetData(output0);
             compute.SetBuffer(0, "InputTensor", outputBuffer);
             compute.SetTexture(0, "OutputTexture", outputTex);
-            compute.SetVector("minValue", new float4(min, 0));
-            compute.SetVector("maxValue", new float4(max, 0));
 
             compute.Dispatch(0, width / 8, height / 8, 1);
             return outputTex;
-        }
-
-        static void FindMinMax(float[,,] arr, out float3 min, out float3 max)
-        {
-            int rows = arr.GetLength(0); // y
-            int cols = arr.GetLength(1); // x
-            int channels = arr.GetLength(2);
-            min = new float3(float.MaxValue, float.MaxValue, float.MaxValue);
-            max = new float3(float.MinValue, float.MinValue, float.MinValue);
-
-            for (int y = 0; y < rows; y++)
-            {
-                for (int x = 0; x < cols; x++)
-                {
-                    float r = arr[y, x, 0];
-                    float g = arr[y, x, 1];
-                    float b = arr[y, x, 2];
-                    min.x = Mathf.Min(r, min.x);
-                    min.y = Mathf.Min(g, min.y);
-                    min.z = Mathf.Min(b, min.z);
-                    max.x = Mathf.Max(r, max.x);
-                    max.y = Mathf.Max(g, max.y);
-                    max.z = Mathf.Max(b, max.z);
-                }
-            }
         }
 
     }
