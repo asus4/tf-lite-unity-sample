b04734fedca9033e1ee7c165f349b4322446d83b
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-07-21 15:49:38 +0200

    WIP hand tracking

--- Packages/com.github.asus4.tflite/Runtime/InterpreterExtension.cs
+++ Packages/com.github.asus4.tflite/Runtime/InterpreterExtension.cs
@@ -18,15 +18,45 @@ using System.Runtime.InteropServices;
 using TfLiteInterpreter = System.IntPtr;
 using TfLiteInterpreterOptions = System.IntPtr;
 using TfLiteRegistration = System.IntPtr;
+using TfLiteContext = System.IntPtr;
+using TfLiteNode = System.IntPtr;
 using TfLiteModel = System.IntPtr;
 using TfLiteTensor = System.IntPtr;
 using TfLiteDelegate = System.IntPtr;
 
 namespace TensorFlowLite
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Registration
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void InitDelegate(TfLiteContext context, IntPtr buffer, UInt64 length);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void FreeDelegate(TfLiteContext context, IntPtr buffer);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate Interpreter.Status PrepareDelegate(TfLiteContext context, TfLiteNode node);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate Interpreter.Status InvokeDelegate(TfLiteContext context, TfLiteNode node);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate string ProfilingStringDelegate(TfLiteContext context, TfLiteNode node);
+
+        public InitDelegate initDelegate;
+        public FreeDelegate freeDelegate;
+        public PrepareDelegate prepareDelegate;
+        public InvokeDelegate invokeDelegate;
+        public ProfilingStringDelegate profilingStringDelegate;
+        public Int32 builtinCode;
+        public string customName;
+        public int version;
+    }
+
     public static class InterpreterExtension
     {
-
+        public static void AddCustomOp(this Interpreter interpreter,string name, Registration registration)
+        {
+            // UnityEngine.Debug.Log("add custom op");
+            // TfLiteInterpreterOptionsAddCustomOp(interpreter, "HOGE");
+        }
 
 #if UNITY_IOS && !UNITY_EDITOR
         private const string TensorFlowLibrary = "__Internal";
