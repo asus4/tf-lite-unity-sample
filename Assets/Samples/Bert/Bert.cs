29a21a544d6437d4f92a3e4fd860aec3c66b69bf
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-07-29 10:24:54 +0200

    Add UIs for Bert QA example

---
+++ Assets/Samples/Bert/Bert.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TensorFlowLite
+{
+
+    public class Bert : IDisposable
+    {
+        Interpreter interpreter;
+
+        int[] inputs0; // input_ids
+        int[] inputs1; // input_mask
+        int[] inputs2; // segment_ids
+        float[] outputs0; // end_logits
+        float[] outputs1; // start_logits
+
+        public Bert(string modelPath, string vocabTable)
+        {
+            // NO GPU
+            var options = new InterpreterOptions()
+            {
+                threads = 2
+            };
+            interpreter = new Interpreter(FileUtil.LoadFile(modelPath), options);
+            interpreter.LogIOInfo();
+
+            const int BUFFER_SIZE = 384;
+            inputs0 = new int[BUFFER_SIZE];
+            inputs1 = new int[BUFFER_SIZE];
+            inputs2 = new int[BUFFER_SIZE];
+            outputs0 = new float[BUFFER_SIZE];
+            outputs1 = new float[BUFFER_SIZE];
+
+            interpreter.AllocateTensors();
+        }
+
+        public void Dispose()
+        {
+            interpreter?.Dispose();
+        }
+    }
+}
---
+++ Assets/Samples/Bert/BertSample.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+public class BertSample : MonoBehaviour
+{
+    [System.Serializable]
+    public class QASet
+    {
+        public string title;
+        public string content;
+        public string[] questions;
+    }
+
+    [System.Serializable]
+    public class QASetCollection
+    {
+        public QASet[] contents;
+    }
+
+    [Header("TFLite")]
+    [SerializeField, FilePopup("*.tflite")] string fileName = "deeplabv3_257_mv_gpu.tflite";
+    [SerializeField] TextAsset qaJson = null;
+    [SerializeField] TextAsset vocabTable = null;
+
+    [Header("UIs")]
+    [SerializeField] Dropdown sentenceDropdown = null;
+    [SerializeField] Text sentenceLabel = null;
+    [SerializeField] Dropdown templetesDropdown = null;
+    [SerializeField] InputField questionInput = null;
+    [SerializeField] Button askButton = null;
+
+    [Header("Data Set")]
+    [SerializeField] QASet[] dataSets;
+
+    Bert bert;
+
+    void Start()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, fileName);
+        bert = new Bert(path, vocabTable.text);
+
+        dataSets = JsonUtility.FromJson<QASetCollection>(qaJson.text).contents;
+
+        // Init UIs
+        sentenceDropdown.ClearOptions();
+        sentenceDropdown.AddOptions(dataSets.Select(o => o.title).ToList());
+        sentenceDropdown.value = 0;
+        sentenceDropdown.onValueChanged.AddListener((value) =>
+        {
+            Debug.Log($"sentence dropdown: {value}");
+            SelectData(dataSets[value]);
+        });
+        SelectData(dataSets[0]);
+
+
+        templetesDropdown.onValueChanged.AddListener((value) =>
+        {
+            questionInput.text = templetesDropdown.captionText.text;
+        });
+        askButton.onClick.AddListener(() =>
+        {
+            var question = questionInput.text;
+            if (string.IsNullOrWhiteSpace(question)) return;
+
+            var currentQa = dataSets[sentenceDropdown.value];
+            Invoke(currentQa, question);
+        });
+    }
+
+    void OnDestroy()
+    {
+        sentenceDropdown.onValueChanged.RemoveAllListeners();
+        templetesDropdown.onValueChanged.RemoveAllListeners();
+        askButton.onClick.RemoveAllListeners();
+        bert.Dispose();
+    }
+
+    void SelectData(QASet qa)
+    {
+        sentenceLabel.text = qa.content;
+        templetesDropdown.ClearOptions();
+        templetesDropdown.AddOptions(qa.questions.ToList());
+        questionInput.text = "";
+    }
+
+    void Invoke(QASet qa, string question)
+    {
+        Debug.Log($"questions: {question}");
+        // TODO bert invoke
+    }
+
+}
