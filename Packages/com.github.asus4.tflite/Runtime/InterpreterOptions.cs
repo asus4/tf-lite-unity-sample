c4d4fdf6400c4c36e836982f676cb590142c1065
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-08-03 12:23:39 +0200

    Add error reporter

--- Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
+++ Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcbc19469b3cdc85ec9df457024176fa736177885b07a4e50d69dbee143ba5ef
-size 4176
+oid sha256:c0c91475d98172b6ba548404132ee87bf00b4b223b680a1202d321a05beef8ad
+size 6411
