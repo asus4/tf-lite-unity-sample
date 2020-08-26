1899bb9c2a8a6ddd03f2fb5057089b181f04f692
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-08-26 10:05:51 +0200

    Arrange the mediapipe sources

---
+++ Assets/Samples/Common/MediaPipe/LowPassFilter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TensorFlowLite
+{
+    /// <summary>
+    /// Super simple low pass filter
+    /// </summary>
+    public class LowPassFilter
+    {
+        public float alpha;
+
+        private float rawValue;
+        private float storedValue;
+        private bool isInitialized;
+
+        public float Apply(float value)
+        {
+            float result;
+            if (isInitialized)
+            {
+                result = alpha * value + (1.0f - alpha) * storedValue;
+            }
+            else
+            {
+                result = value;
+                isInitialized = true;
+            }
+            rawValue = value;
+            storedValue = result;
+            return result;
+        }
+
+        public float Apply(float value, float alpha)
+        {
+            this.alpha = alpha;
+            return Apply(value);
+        }
+
+    }
+}
