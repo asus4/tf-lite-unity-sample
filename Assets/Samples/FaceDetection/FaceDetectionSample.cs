2ecdf04393ec489b9e025a4997ba81927e0ca892
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-08-27 11:36:00 +0200

    Add FaceDetect scene

---
+++ Assets/Samples/FaceDetection/FaceDetectionSample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TensorFlowLite;
+
+/// <summary>
+/// BlazeFace from MediaPile
+/// https://github.com/google/mediapipe
+/// https://viz.mediapipe.dev/demo/face_detection
+/// </summary>
+public class FaceDetectionSample : MonoBehaviour
+{
+    [SerializeField, FilePopup("*.tflite")] string faceModelFile = "coco_ssd_mobilenet_quant.tflite";
+    [SerializeField] RawImage cameraView = null;
+
+    WebCamTexture webcamTexture;
+    FaceDetect faceDetect;
+    PrimitiveDraw draw;
+
+    void Start()
+    {
+        string detectionPath = Path.Combine(Application.streamingAssetsPath, faceModelFile);
+        faceDetect = new FaceDetect(detectionPath);
+
+        string cameraName = WebCamUtil.FindName(new WebCamUtil.PreferSpec()
+        {
+            isFrontFacing = false,
+            kind = WebCamKind.WideAngle,
+        });
+        webcamTexture = new WebCamTexture(cameraName, 1280, 720, 30);
+        cameraView.texture = webcamTexture;
+        webcamTexture.Play();
+        Debug.Log($"Starting camera: {cameraName}");
+
+        draw = new PrimitiveDraw()
+        {
+            color = Color.blue,
+        };
+    }
+
+    void Update()
+    {
+        faceDetect.Invoke(webcamTexture);
+        cameraView.material = faceDetect.transformMat;
+    }
+}
