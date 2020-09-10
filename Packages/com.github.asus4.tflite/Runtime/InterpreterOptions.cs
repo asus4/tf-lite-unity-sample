c548390df93d2f021e38d3b625ea558fa0c1dcc1
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-09-11 01:43:06 +0200

    Change the error reporter's log level to Warning
    ref #66

--- Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
+++ Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:609f474d82b960da38217b1501c33894023bbdecd06bfceb35834aacadd13acf
-size 6437
+oid sha256:52f2b8404355ce7e600b79df906e87a5fdf03ad3e5becd894aa28a423d24023b
+size 6465
