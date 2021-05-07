2c16101f943b5da85c91cf2f8807b2cae9a8302d
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-05-08 00:17:18 +0200

    Split error reporter

---
+++ Packages/com.github.asus4.tflite/Runtime/ErrorReporter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7147c2a81e9af2efbdbcf73d9a152349dbb3982f04307fc0a3b33417e0619bf
+size 3434
--- Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
+++ Packages/com.github.asus4.tflite/Runtime/InterpreterOptions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c296b863a1eac29c4d6d68042ed6c70f25d9d16c6a01ff95c87c850d835084e
-size 6454
+oid sha256:c9bcbc1baa97e9ea0cca0f9271ef2f4ada4335191995b554393dbaf13855dd7f
+size 4491
