a40c82aff44e145d19ecea0bea8eb010e8f7065b
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-05-20 00:46:25 +0200

    Refactor

--- Packages/com.github.asus4.tflite/Runtime/Interpreter.cs
+++ Packages/com.github.asus4.tflite/Runtime/Interpreter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0287407222c8a2c15fb8baa2c257ea5c2bc4e885b6f0b1cb8a751b254d2866b4
-size 11369
+oid sha256:e6a0c838836afded42a44ec12c6f8108747e288cb0c208d59c39796a04d15a74
+size 11359
