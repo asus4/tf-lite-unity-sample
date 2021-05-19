506d2bc5cbd991602dafb427083c9869985a452c
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-05-19 21:30:39 +0200

    Rename extern method names

--- Packages/com.github.asus4.tflite/Runtime/GpuDelegateV2.cs
+++ Packages/com.github.asus4.tflite/Runtime/GpuDelegateV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:285845ab37b53752465da6815a52dedb70037ab1ff47fb46ed2fe4c12d553a85
-size 5253
+oid sha256:1bac9638568aa175768063658035baf15ceea3d82d007796e9e261bca554ca1d
+size 5229
