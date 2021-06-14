96e42e2c1c81a94b80e298125cdda57299fcd8e3
Koki Ibukuro <koki.ibukuro@gmail.com>
2021-06-14 17:26:51 +0200

    Copy webcam name, a custom name attribute
    from mock-arfoundation
    https://github.com/asus4/mock-arfoundation/blob/main/Packages/MockARFoundation/Editor/WebcamNameDrawer.cs

---
+++ Packages/com.github.asus4.tflite.common/Runtime/WebCamNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TensorFlowLite
+{
+    /// <summary>
+    /// A string attribute to select WebCam name in Editor
+    /// </summary>
+    public class WebCamName : PropertyAttribute
+    {
+        public WebCamName()
+        {
+        }
+    }
+}
