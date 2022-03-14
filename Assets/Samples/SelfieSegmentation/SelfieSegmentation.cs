b8b2517bddef7f9c3b7a4a9c26f17181355d246d
Koki Ibukuro <koki.ibukuro@gmail.com>
2022-03-14 02:45:30 +0100

    Add post-process

--- Assets/Samples/SelfieSegmentation/SelfieSegmentation.cs
+++ Assets/Samples/SelfieSegmentation/SelfieSegmentation.cs
@@ -11,10 +11,17 @@ namespace TensorFlowLite
         private readonly ComputeShader compute;
         private ComputeBuffer labelBuffer;
         private RenderTexture labelTex;
+        private RenderTexture maskTex;
+
+        private readonly int kLabelToTex;
+        private readonly int kBilateralFilter;
+        private static readonly int kLabelBuffer = Shader.PropertyToID("LabelBuffer");
+        private static readonly int kInputTexture = Shader.PropertyToID("InputTexture");
+        private static readonly int kOutputTexture = Shader.PropertyToID("OutputTexture");
 
         public SelfieSegmentation(string modelPath, ComputeShader compute) : base(modelPath, true)
         {
-            var odim0 = interpreter.GetOutputTensorInfo(0).shape;
+            int[] odim0 = interpreter.GetOutputTensorInfo(0).shape;
 
             Debug.Assert(odim0[1] == height);
             Debug.Assert(odim0[2] == width);
@@ -25,10 +32,18 @@ namespace TensorFlowLite
             compute.SetInt("Width", width);
             compute.SetInt("Height", height);
 
+            labelBuffer = new ComputeBuffer(height * width, sizeof(float) * 2);
+
             labelTex = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
             labelTex.enableRandomWrite = true;
             labelTex.Create();
-            labelBuffer = new ComputeBuffer(height * width, sizeof(float) * 2);
+
+            maskTex = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
+            maskTex.enableRandomWrite = true;
+            maskTex.Create();
+
+            kLabelToTex = compute.FindKernel("LabelToTex");
+            kBilateralFilter = compute.FindKernel("BilateralFilter");
         }
 
         public override void Dispose()
@@ -39,6 +54,12 @@ namespace TensorFlowLite
                 labelTex.Release();
                 Object.Destroy(labelTex);
                 labelTex = null;
+            }
+            if (maskTex != null)
+            {
+                maskTex.Release();
+                Object.Destroy(maskTex);
+                maskTex = null;
             }
 
             labelBuffer?.Release();
@@ -59,12 +80,14 @@ namespace TensorFlowLite
         public RenderTexture GetResultTexture()
         {
             labelBuffer.SetData(output0);
-            compute.SetBuffer(0, "LabelBuffer", labelBuffer);
-            compute.SetTexture(0, "Result", labelTex);
+            compute.SetBuffer(kLabelToTex, kLabelBuffer, labelBuffer);
+            compute.SetTexture(kLabelToTex, kOutputTexture, labelTex);
+            compute.Dispatch(kLabelToTex, width / 8, height / 8, 1);
 
-            compute.Dispatch(0, width / 8, height / 8, 1);
-
-            return labelTex;
+            compute.SetTexture(kBilateralFilter, kInputTexture, labelTex);
+            compute.SetTexture(kBilateralFilter, kOutputTexture, maskTex);
+            compute.Dispatch(kBilateralFilter, width / 8, height / 8, 1);
+            return maskTex;
         }
     }
 }
