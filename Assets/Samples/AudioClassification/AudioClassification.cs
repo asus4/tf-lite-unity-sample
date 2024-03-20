7cfeba63a9aef89d8312dbdff1cc1f243efa68af
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-03-20 14:12:36 +0100

    Implement audio classification

--- Assets/Samples/AudioClassification/AudioClassification.cs
+++ Assets/Samples/AudioClassification/AudioClassification.cs
@@ -1,14 +1,39 @@
+using System;
 using System.Linq;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
 
 namespace TensorFlowLite
 {
-    public sealed class AudioClassification : System.IDisposable
+    public sealed class AudioClassification : IDisposable
     {
+        public readonly struct Label : IComparable<Label>
+        {
+            public readonly int id;
+            public readonly float score;
+
+            public Label(int id, float score)
+            {
+                this.id = id;
+                this.score = score;
+            }
+
+            public int CompareTo(Label other)
+            {
+                return other.score.CompareTo(score);
+            }
+        }
+
         private readonly Interpreter interpreter;
         private readonly float[] input;
         private readonly float[] output;
+        private NativeArray<Label> labels;
+
 
         public float[] Input => input;
+        public float[] Output => output;
 
         public AudioClassification(byte[] modelData)
         {
@@ -20,12 +45,15 @@ namespace TensorFlowLite
             // Allocate IO buffers
             input = new float[interpreter.GetInputTensorInfo(0).shape.Aggregate((x, y) => x * y)];
             output = new float[interpreter.GetOutputTensorInfo(0).shape.Aggregate((x, y) => x * y)];
+
+            labels = new NativeArray<Label>(output.Length, Allocator.Persistent);
             interpreter.AllocateTensors();
         }
 
         public void Dispose()
         {
             interpreter?.Dispose();
+            labels.Dispose();
         }
 
         public void Run()
@@ -33,6 +61,36 @@ namespace TensorFlowLite
             interpreter.SetInputTensorData(0, input);
             interpreter.Invoke();
             interpreter.GetOutputTensorData(0, output);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                labels[i] = new Label(i, output[i]);
+            }
+
+        }
+
+        public NativeSlice<Label> GetTopLabels(int topK)
+        {
+            labels.Sort();
+            return labels.Slice(0, topK);
+        }
+
+        [BurstCompile]
+        internal struct OutPutToLabel : IJob
+        {
+            [ReadOnly]
+            public NativeSlice<float> input;
+
+            [WriteOnly]
+            public NativeSlice<Label> output;
+
+            public void Execute()
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    output[i] = new Label(i, input[i]);
+                }
+            }
         }
     }
 }
--- Assets/Samples/AudioClassification/AudioClassificationSample.cs
+++ Assets/Samples/AudioClassification/AudioClassificationSample.cs
@@ -1,31 +1,60 @@
 using System.Collections;
+using System.Text;
 using TensorFlowLite;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public sealed class AudioClassificationSample : MonoBehaviour
 {
+    [Header("Configs")]
     [SerializeField, FilePopup("*.tflite")]
     private string modelFile = string.Empty;
 
     [SerializeField]
-    private TextAsset labels;
+    private TextAsset labelFile;
 
     [SerializeField]
-    private Text resultText = null;
+    [Range(0.1f, 5f)]
+    private float runEachNSec = 0.2f;
+
+
+    [SerializeField]
+    [Range(1, 10)]
+    private int showTopKLabels = 3;
 
     [SerializeField]
     private MicrophoneBuffer.Options microphoneOptions = new();
 
+    [Header("UI")]
+    [SerializeField]
+    private Text resultText = null;
+
+    [SerializeField]
+    private RectTransform waveformView = null;
+
+
     private AudioClassification classification;
     private MicrophoneBuffer mic;
+    private string[] labelNames;
+    private PrimitiveDraw waveFormDrawer;
+    private readonly Vector3[] rtCorners = new Vector3[4];
+    private readonly StringBuilder sb = new();
 
     private IEnumerator Start()
     {
+        labelNames = labelFile.text.Split('\n');
         classification = new AudioClassification(FileUtil.LoadFile(modelFile));
+        waveFormDrawer = new PrimitiveDraw(Camera.main, gameObject.layer);
 
         mic = new MicrophoneBuffer();
         yield return mic.StartRecording(microphoneOptions);
+
+        while (Application.isPlaying)
+        {
+            yield return new WaitForSeconds(runEachNSec);
+            Run();
+        }
     }
 
     private void OnDestroy()
@@ -34,11 +63,49 @@ public sealed class AudioClassificationSample : MonoBehaviour
         mic?.Dispose();
     }
 
+    private void Update()
+    {
+        waveFormDrawer.Apply(drawEditor: true, clear: false);
+    }
+
     private void Run()
     {
         if (!mic.IsRecording) return;
 
         mic.GetLatestSamples(classification.Input);
         classification.Run();
+
+        sb.Clear();
+        sb.AppendLine($"Top {showTopKLabels}:");
+        var labels = classification.GetTopLabels(showTopKLabels);
+        for (int i = 0; i < labels.Length; i++)
+        {
+            sb.AppendLine($"{labelNames[labels[i].id]}: {labels[i].score * 100f:F1}%");
+        }
+        resultText.text = sb.ToString();
+
+        UpdateWaveform(classification.Input);
+    }
+
+    private void UpdateWaveform(float[] input)
+    {
+        waveformView.GetWorldCorners(rtCorners);
+        float3 min = rtCorners[0];
+        float3 max = rtCorners[2];
+
+        waveFormDrawer.Clear();
+        waveFormDrawer.color = Color.green;
+
+        int length = input.Length;
+        float delta = 1f / length;
+
+        Vector3 prev = math.lerp(min, max, new float3(0, input[0] * 0.5f + 0.5f, 0));
+        for (int i = 1; i < length; i++)
+        {
+            float3 t = new(i * delta, input[i] * 0.5f + 0.5f, 0);
+            Vector3 point = math.lerp(min, max, t);
+            waveFormDrawer.Line3D(prev, point, 0.01f);
+            prev = point;
+        }
     }
 }
