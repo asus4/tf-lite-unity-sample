7d9a7f0646a025ff7bcadcda339233b34a3375bf
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-06-25 12:38:20 +0200

    Cleanup using

--- Assets/Samples/Bert/BertSample.cs
+++ Assets/Samples/Bert/BertSample.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TensorFlowLite;
