9214d8964f3f32f42a57f233d627fa9067d502d2
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-06-25 11:49:18 +0200

    Implement LoadFileAsync in FileUtil

--- Packages/com.github.asus4.tflite.common/Runtime/FileUtil.cs
+++ Packages/com.github.asus4.tflite.common/Runtime/FileUtil.cs
@@ -1,12 +1,41 @@
 using System.IO;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Networking;
+#if TFLITE_UNITASK_ENABLED
+using Cysharp.Threading.Tasks;
+#endif
 
 namespace TensorFlowLite
 {
     public static class FileUtil
     {
         public static byte[] LoadFile(string path)
+        {
+            string uri = GetStreamingAssetsUri(path);
+            using (var request = UnityWebRequest.Get(uri))
+            {
+                request.SendWebRequest();
+                while (!request.isDone)
+                {
+                }
+                return request.downloadHandler.data;
+            }
+        }
+
+#if TFLITE_UNITASK_ENABLED
+        public static async UniTask<byte[]> LoadFileAsync(string path, CancellationToken cancellationToken = default)
+        {
+            string uri = GetStreamingAssetsUri(path);
+            using (var request = UnityWebRequest.Get(uri))
+            {
+                await request.SendWebRequest().WithCancellation(cancellationToken);
+                return request.downloadHandler.data;
+            }
+        }
+#endif
+
+        static string GetStreamingAssetsUri(string path)
         {
             if (!IsPathRooted(path))
             {
@@ -17,14 +46,7 @@ namespace TensorFlowLite
             {
                 path = "file://" + path;
             }
-            using (var request = UnityWebRequest.Get(path))
-            {
-                request.SendWebRequest();
-                while (!request.isDone)
-                {
-                }
-                return request.downloadHandler.data;
-            }
+            return path;
         }
 
         static bool IsPathRooted(string path)
